22955710c9d05b06670b7da0239a8822699e7fb1
choi-jihun <cjh0630@naver.com>
2025-01-27 17:38:54 +0900

    feat: dummydata와 class추가

---
+++ lib/cosntants/dummy_dictionary_info.dart
@@ -0,0 +1,23 @@
+import 'package:whatisthis/model/dictinary_info.dart';
+
+final List<DictinaryInfo> dummyDictionaryInfos = [
+  DictinaryInfo(
+    dicId: 1,
+    imageUrl: [
+      'https://i.namu.wiki/i/TYxKQDnuwFOcxdSaPR-L81SPQGf5aPEz13tINJ-Z508LKNtGmRmkZTKKEN82SrIZAYoLL8WSbXGzv2PiLgpRSg.webp',
+      'https://i.namu.wiki/i/TYxKQDnuwFOcxdSaPR-L81SPQGf5aPEz13tINJ-Z508LKNtGmRmkZTKKEN82SrIZAYoLL8WSbXGzv2PiLgpRSg.webp',
+    ],
+    dicName: '은행나무',
+    dicDes:
+        '은행나무는 은행나무과의 교목으로, 수명이 길고 병충해에 강해 가로수로 많이 심습니다. 가을이 되면 노란색으로 물들어 아름다운 풍경을 만듭니다. 암나무는 은행열매를 맺으며, 이는 한방과 식용으로 사용됩니다.',
+  ),
+  DictinaryInfo(
+    dicId: 2,
+    imageUrl: [
+      'https://i.namu.wiki/i/TYxKQDnuwFOcxdSaPR-L81SPQGf5aPEz13tINJ-Z508LKNtGmRmkZTKKEN82SrIZAYoLL8WSbXGzv2PiLgpRSg.webp',
+    ],
+    dicName: '벚나무',
+    dicDes:
+        '벚나무는 장미과의 낙엽교목으로, 봄에 피는 벚꽃으로 유명합니다. 한국, 일본 등 동아시아가 원산지이며, 공원과 가로수로 널리 심어져 있습니다. 봄이 되면 분홍빛 꽃을 피워 상춘객들의 사랑을 받고 있습니다.',
+  ),
+];
---
+++ lib/model/dictinary_info.dart
@@ -0,0 +1,13 @@
+class DictinaryInfo {
+  final int dicId;
+  final List<String> imageUrl;
+  final String dicName;
+  final String dicDes;
+
+  DictinaryInfo({
+    required this.dicId,
+    required this.imageUrl,
+    required this.dicName,
+    required this.dicDes,
+  });
+}
