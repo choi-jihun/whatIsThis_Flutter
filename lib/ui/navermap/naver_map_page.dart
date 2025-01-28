d9322ab0ad1a7203fc4a08bdff85cd6446d23074
choi-jihun <cjh0630@naver.com>
2025-01-28 17:30:46 +0900

    feature: 필요한 버튼들 추가

--- lib/ui/navermap/naver_map_page.dart
+++ lib/ui/navermap/naver_map_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:whatisthis/ui/confirm_dialog.dart';
+import 'package:whatisthis/ui/dictionary/dictionary_page.dart';
+import 'package:whatisthis/ui/map_button.dart';
+import 'package:whatisthis/ui/mission_dialog.dart';
 
 class NaverMapPage extends StatelessWidget {
   const NaverMapPage({super.key});
@@ -18,12 +22,84 @@ class NaverMapPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: NaverMap(
-          options: const NaverMapViewOptions(locationButtonEnable: true),
-          onMapReady: (controller) async {
-            await _checkPermission();
-            controller.setLocationTrackingMode(NLocationTrackingMode.follow);
-          },
+        body: Stack(
+          children: [
+            NaverMap(
+              options: const NaverMapViewOptions(locationButtonEnable: true),
+              onMapReady: (controller) async {
+                await _checkPermission();
+                controller
+                    .setLocationTrackingMode(NLocationTrackingMode.follow);
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24),
+              child: Column(
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        MapButton(
+                          onTap: () => showDialog(
+                            context: context,
+                            builder: (BuildContext context) =>
+                                const ConfirmDialog(),
+                          ),
+                          text: '탐험 끝내기',
+                          backgroundColor: Colors.green[600],
+                          isLeftRadius: false,
+                        ),
+                        MapButton(
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  DictionaryPage(), //TODO 추후에 parkId 혹은 adventureId 보내기
+                            ),
+                          ),
+                          text: '도감',
+                          icon: Icons.book,
+                          isRightRadius: false,
+                        ),
+                      ]),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      MapButton(
+                        onTap: () => showDialog(
+                            context: context,
+                            builder: (BuildContext context) => MissionDialog()),
+                        text: '도전과제',
+                        icon: Icons.question_mark_sharp,
+                        isRightRadius: false,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 16,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green[600],
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(16)),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.center_focus_weak),
+                    iconSize: 42,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
