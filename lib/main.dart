9f1f70201f2bc3aac8590d63a6721e61a92fb18c
choi-jihun <cjh0630@naver.com>
2025-02-09 22:37:28 +0900

    feat: uspabase 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:whatisthis/theme/app_theme.dart';
+import 'package:whatisthis/ui/login/login_page.dart';
 import 'package:whatisthis/ui/mainScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await NaverMapSdk.instance.initialize(clientId: dotenv.env['NAVER_MAP_KEY']);
-  runApp(const MyApp());
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: const Mainscreen(),
+      home: StreamBuilder<AuthState>(
+        stream: Supabase.instance.client.auth.onAuthStateChange,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final authState = snapshot.data!;
+            if (authState.session != null) {
+              return const Mainscreen();
+            }
+          }
+          return LoginPage();
+        },
+      ),
     );
   }
 }
