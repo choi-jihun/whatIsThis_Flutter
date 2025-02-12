282945f6f43e2d73d1ce3e7fa4e248acd5bb5ca0
choi-jihun <cjh0630@naver.com>
2025-02-13 01:12:15 +0900

    chore: season을 nullable하게 수정

--- lib/model/species.dart
+++ lib/model/species.dart
@@ -2,13 +2,13 @@ class Species {
   final String id;
   final String name;
   final String? imageUrl;
-  final String season;
+  final String? season;
 
   Species({
     required this.id,
     required this.name,
     this.imageUrl,
-    required this.season,
+    this.season,
   });
 
   factory Species.fromJson(Map<String, dynamic> json) {
@@ -16,7 +16,7 @@ class Species {
       id: json['species_id'],
       name: json['name'],
       imageUrl: json['image_url'],
-      season: json['season'],
+      season: json['season']?.toString(),
     );
   }
 }
