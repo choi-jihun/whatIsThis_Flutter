b14f51dd334ee11c41d0a933f136276f342a69e0
choi-jihun <cjh0630@naver.com>
2025-02-13 22:26:50 +0900

    chore: description추가

--- lib/model/species.dart
+++ lib/model/species.dart
@@ -3,20 +3,21 @@ class Species {
   final String name;
   final String? imageUrl;
   final String? season;
+  final String? description;
 
-  Species({
-    required this.id,
-    required this.name,
-    this.imageUrl,
-    this.season,
-  });
+  Species(
+      {required this.id,
+      required this.name,
+      this.imageUrl,
+      this.season,
+      this.description});
 
   factory Species.fromJson(Map<String, dynamic> json) {
     return Species(
-      id: json['species_id'],
-      name: json['name'],
-      imageUrl: json['image_url'],
-      season: json['season']?.toString(),
-    );
+        id: json['species_id'],
+        name: json['name'],
+        imageUrl: json['image_url'],
+        season: json['season']?.toString(),
+        description: json['description']?.toString());
   }
 }
